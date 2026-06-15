53769893da3a0b701c1e1e7ad79d30be1116c43e
Layi <kidfob@gmail.com>
2026-06-14 21:15:17 -0500

    P-pre: fix offscreen GPU row origin

--- platform/qt/GpuPreviewProcessing.cpp
+++ platform/qt/GpuPreviewProcessing.cpp
@@ -582,7 +582,8 @@ QByteArray gpuPreviewProcessingSubsetFragmentShaderSource(void)
         "}\n"
         "void main()\n"
         "{\n"
-        "    vec4 sampledColor = texture2D(frameTexture, vTexCoord);\n"
+        "    vec2 sourceCoord = vec2(vTexCoord.x, 1.0 - vTexCoord.y);\n"
+        "    vec4 sampledColor = texture2D(frameTexture, sourceCoord);\n"
         "    gl_FragColor = vec4(applyPreviewProcessing(sampledColor.rgb), sampledColor.a);\n"
         "}\n");
 }
